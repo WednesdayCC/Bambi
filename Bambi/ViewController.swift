30f8dcca6fac4cb754df0bf119369fb19f4e4fce
AzureChen <azure517981@gmail.com>
2016-01-17 21:35:33 +0800

    Init.

---
+++ Bambi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bambi
+//
+//  Created by Azure Chen on 2016/1/16.
+//  Copyright © 2016年 WednesdayCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
